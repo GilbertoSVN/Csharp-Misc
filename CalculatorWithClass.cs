cff4b71d66fdcb01cf74a3e50ff1da7c3054f0d0
Gilberto VN <36732280+GilbertoSVN@users.noreply.github.com>
2019-03-11 17:34:24 -0300

    Updated Calculator to version v0.2
    - Added history tracking.
    - Added loop for progressive use.

--- CalculatorWithClass.cs
+++ CalculatorWithClass.cs
@@ -1,5 +1,5 @@
-﻿/*  Calculadora v.0.1
- * Funções de Soma, Subtração, Multiplicação, Divisão, Potência e Resto.
+/*  Calculadora v.0.2
+ * Funções de Soma, Subtração, Multipicação, Divisão, Potência e Resto.
  * Uso de Funções e Classe como método de estudo/resumo de aplicação.
  * @@@@@@@@@@@@@@     @@@      @@@
  * @@@@@@@@@@@@@@     @@@      @@@
@@ -14,6 +14,7 @@
  */
 
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace CalculatorApp
@@ -25,63 +26,70 @@ namespace CalculatorApp
             string inputNumOne, inputNumTwo, inputOperator; //Variáveis do programa
             Calculator Calc = new Calculator(); //Declarando Calculadora
 
-            Console.WriteLine("<||||| Calculadora Básica v0.1 |||||>\n");
-
-            Console.Write("Digite o primeiro número: ");
-            inputNumOne = Console.ReadLine(); //Recebendo primeiro número
-
-            while (!Calc.checkNumber(inputNumOne)) //Validação do primeiro número
+            while (true)
             {
                 Console.Clear();
-                Console.WriteLine("Número digitado inválido, digite um número válido!");
-                Console.Write("Digite o primeiro número: ");
-                inputNumOne = Console.ReadLine();
+                Calc.printStart();
+
+                Calc.printHistory(); //Imprime histórico, se existente
+
+                Console.WriteLine("Para começar, digite a operação desejada:");
+                Console.WriteLine("Operações disponíveis:\n + (Soma), - (Subtração), / (Divisão)\n * (Multiplicação), ^ (Potêcia)\n % (Resto da Divisão)");
+                Console.WriteLine("Digite . para encerrar o programa");
+
+                do
+                {
+                    Console.Write("Digite a opção desejada: ");
+                    inputOperator = Console.ReadLine(); //Recebendo operador
+
+                } while (!Calc.checkOperator(inputOperator)); //Validação do operador
+
+                if (inputOperator == ".") //Verifica se operador é '.': encerra o programa
+                {
+                    return;
+                }
+
+                do
+                {
+                    Console.Write("Digite o primeiro número: ");
+                    inputNumOne = Console.ReadLine(); //Recebendo primeiro número
+
+                } while (!Calc.checkNumber(inputNumOne)); //Validação do primeiro número
+
+                do
+                {
+                    Console.Write("Digite o segundo número: ");
+                    inputNumTwo = Console.ReadLine(); //Recebendo segundo número
+
+                } while (!Calc.checkNumber(inputNumTwo)); //Validação do segundo número
+
+                if (inputOperator == "/" && inputNumTwo == "0") //Verifica se existe divisão por 0
+                {
+                    Console.Clear();
+                    Console.WriteLine("Não é possível dividir por ZERO! - Encerrando programa.");
+                }
+                else
+                {
+                    //Realiza o cálculo e apresenta o resultado
+                    Console.WriteLine("O resultado da operação é:");
+                    Console.WriteLine(inputNumOne + " " + inputOperator + " " + inputNumTwo + " = " + Calc.calcOperation(Convert.ToDecimal(inputNumOne), Convert.ToDecimal(inputNumTwo), inputOperator));
+                }
+
+                //Adiciona valore ao histórico dentro da classe
+                Calc.histNumOne.Add(Convert.ToDecimal(inputNumOne));
+                Calc.histNumTwo.Add(Convert.ToDecimal(inputNumTwo));
+                Calc.histOperator.Add(inputOperator);
+
+                Calc.sessionID += 1;
+
+                Console.WriteLine("Digite qualquer coisa para continuar na Calculadora\nDigite . para encerrar o programa");
+                inputOperator = Console.ReadLine(); //Reutilizando variável para dar continuidade ao programa.
+
+                if (inputOperator == ".") //Verifica se operador é '.': encerra o programa
+                {
+                    return;
+                }
             }
-
-            Console.Clear();
-            Console.Write("Digite o segundo número: "); 
-            inputNumTwo = Console.ReadLine(); //Recebendo segundo número
-
-            while (!Calc.checkNumber(inputNumTwo)) //Validação do segundo número
-            {
-                Console.Clear();
-                Console.WriteLine("Número digitado inválido, digite um número válido!");
-                Console.Write("Digite o segundo número: ");
-                inputNumTwo = Console.ReadLine();
-            }
-
-            Console.Clear();
-            Console.WriteLine("(Operações disponíveis: +, -, /, *, ^, %)");
-            Console.Write("Digite a operação: ");
-            inputOperator = Console.ReadLine(); //Recebendo operador
-
-            while (!Calc.checkOperator(inputOperator)) //Validação do operador
-            {
-                Console.Clear();
-                Console.WriteLine("Operação inválida, digite uma operação válida!");
-
-                inputOperator = Console.ReadLine();
-            }
-
-            if (inputOperator == ".") //Verifica se operador é '.': encerra o programa
-            {
-                return;
-            }
-            else if (inputOperator == "/" && inputNumTwo == "0") //Verifica se existe divisão por 0
-            {
-                Console.Clear();
-                Console.WriteLine("Não é possível dividir por ZERO! - Encerrando programa.");
-            }
-            else
-            {
-                //Realiza o cálculo e apresenta o resultado
-                Console.Clear();
-                Console.WriteLine("O resultado da operação é:");
-                Console.WriteLine(inputNumOne + " " + inputOperator + " " + inputNumTwo + " = " + (decimal) Calc.calcOperation(Convert.ToDecimal(inputNumOne), Convert.ToDecimal(inputNumTwo), inputOperator));
-            }
-
-            Console.ReadKey();
-
         }
     }
 }
@@ -89,6 +97,35 @@ namespace CalculatorApp
 //Classe criada para abrigar funções da calculadora
 public class Calculator
 {
+    public int sessionID { get; set; }
+    public List<decimal> histNumOne { get; set; }
+    public List<decimal> histNumTwo { get; set; }
+    public List<string> histOperator { get; set; }
+
+    public void printStart()
+    {
+        Console.WriteLine(" ________________________");
+        Console.WriteLine("|[   Calculadora v.0.2  ]|");
+        Console.WriteLine("|[ 1 ] [ 2 ] [ 3 ]  [ + ]|");
+        Console.WriteLine("|[ 4 ] [ 5 ] [ 6 ]  [ - ]|");
+        Console.WriteLine("|[ 7 ] [ 8 ] [ 9 ]  [ * ]|");
+        Console.WriteLine("|[ ^ ] [ 0 ] [ % ]  [ / ]|");
+        Console.WriteLine("|________________________|\n");
+    }
+
+    public void printHistory()
+    {
+        if (sessionID > 1)
+        {
+            Console.WriteLine("---------------------------");
+            Console.WriteLine("         HISTÓRICO         ");
+            for (var i = 0; i < sessionID - 1; i++)
+            {
+                Console.WriteLine("{0} {1} {2} = {3}",  histNumOne[i], histOperator[i], histNumTwo[i], calcOperation(histNumOne[i], histNumTwo[i], histOperator[i]));
+            }
+            Console.WriteLine("---------------------------");
+        }
+    }
 
     public bool checkNumber(string testNum)
     {
@@ -106,7 +143,7 @@ public class Calculator
     public object calcOperation(decimal nOne, decimal nTwo, string operation) //Declarado como object para permitir retorno textual
     {
         double res;
-        switch(operation)
+        switch (operation)
         {
             case "+":
                 res = (double)(nOne + nTwo);
@@ -127,9 +164,17 @@ public class Calculator
                 res = (double)(nOne % nTwo);
                 break;
             default:
-            return "Não foi possível realizar essa operação.";
+                return "Não foi possível realizar essa operação.";
         }
 
         return res;
+    }
+
+    public Calculator()
+    {
+        sessionID = 1;
+        histNumOne = new List<decimal>();
+        histNumTwo = new List<decimal>();
+        histOperator = new List<string>();
     }
 }
